91b787de0053e89f8852d3b4b3e34f1c0f13bf4c
Vincent Botoku <vbotoku@grupolvt.com>
2024-01-10 12:46:34 -0600

    feat:highlights active section in Header

--- components/sharedComponents/Header.tsx
+++ components/sharedComponents/Header.tsx
@@ -5,15 +5,15 @@ import { ContentDisplayContext } from '../helpers/ContextProvider'
 type Props = {}
 
 const Header = (props: Props) => {
-const {setSection} = useContext(ContentDisplayContext)
+const {setSection, activeSection} = useContext(ContentDisplayContext)
   return (
     <div className=''>
-        <ul className=' flex lg:flex-col gap-1 sm:gap-2 border-lime-100 border-b-2 mb-2 lg:border-b-0 lg:border-r-2'>
-            <li onClick={() =>setSection && setSection('todo')}>To Do</li>
-            <li onClick={() => setSection && setSection('tobuy')}>To Buy</li>
-            <li onClick={() => setSection && setSection('expenses')}>Expenses</li>
-            <li onClick={() => setSection && setSection('meals')}>Meals</li>
-            <li onClick={() => setSection && setSection('ideas')}>Ideas</li>
+        <ul className='px-2 flex lg:flex-col gap-1 sm:gap-2 border-lime-100 border-b-2 mb-2 lg:border-b-0 lg:border-r-2'>
+            <li className={`cursor-pointer w-max rounded-xl px-2 py-1 hover:text-purple-400 ${activeSection === "todo" && "bg-purple-700"}`} onClick={() =>setSection && setSection('todo')}>To Do</li>
+            <li className={`cursor-pointer w-max rounded-xl px-2 py-1 hover:text-purple-400 ${activeSection === "tobuy" && "bg-purple-700"}`} onClick={() => setSection && setSection('tobuy')}>To Buy</li>
+            <li className={`cursor-pointer w-max rounded-xl px-2 py-1 hover:text-purple-400 ${activeSection === "expenses" && "bg-purple-700"}`} onClick={() => setSection && setSection('expenses')}>Expenses</li>
+            <li className={`cursor-pointer w-max rounded-xl px-2 py-1 hover:text-purple-400 ${activeSection === "meals" && "bg-purple-700"}`} onClick={() => setSection && setSection('meals')}>Meals</li>
+            <li className={`cursor-pointer w-max rounded-xl px-2 py-1 hover:text-purple-400 ${activeSection === "ideas" && "bg-purple-700"}`} onClick={() => setSection && setSection('ideas')}>Ideas</li>
         </ul>
     </div>
   )
