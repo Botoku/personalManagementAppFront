be62b9e6bff8e46a65cf839a456356b79993c33f
Vincent Botoku <vbotoku@grupolvt.com>
2024-02-12 12:55:29 -0600

    feat: switches fetch url to remote url

--- components/listComponents/TodoUploadForm.tsx
+++ components/listComponents/TodoUploadForm.tsx
@@ -13,7 +13,7 @@ const TodoUploadForm = (props: Props) => {
     if (user?.id)
       axios
         .post(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL_LOCAL}/todo`,
+          `${process.env.NEXT_PUBLIC_BACKEND_URL_REMOTE}/todo`,
           {
             name: todo,
             dateDue: theDate,
